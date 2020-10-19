6406b81c190be9b7cd299c9747e052c8c968d99e
Loïc Lefèvre <loic.lefevre@gmail.com>
2020-10-19 15:31:16 +0200

    version 1.0.2

--- src/main/java/com/oracle/dragon/DragonStack.java
+++ src/main/java/com/oracle/dragon/DragonStack.java
@@ -1,44 +1,8 @@
 package com.oracle.dragon;
 
-import com.oracle.bmc.ConfigFileReader;
-import com.oracle.bmc.auth.AuthenticationDetailsProvider;
-import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
-import com.oracle.bmc.database.DatabaseClient;
-import com.oracle.bmc.database.DatabaseWaiters;
-import com.oracle.bmc.database.model.*;
-import com.oracle.bmc.database.requests.*;
-import com.oracle.bmc.database.responses.*;
-import com.oracle.bmc.identity.IdentityClient;
-import com.oracle.bmc.identity.requests.GetUserRequest;
-import com.oracle.bmc.identity.responses.GetUserResponse;
 import com.oracle.bmc.model.BmcException;
-import com.oracle.bmc.objectstorage.ObjectStorage;
-import com.oracle.bmc.objectstorage.ObjectStorageClient;
-import com.oracle.bmc.objectstorage.model.BucketSummary;
-import com.oracle.bmc.objectstorage.model.CreateBucketDetails;
-import com.oracle.bmc.objectstorage.requests.CreateBucketRequest;
-import com.oracle.bmc.objectstorage.requests.GetNamespaceRequest;
-import com.oracle.bmc.objectstorage.requests.ListBucketsRequest;
-import com.oracle.bmc.objectstorage.responses.CreateBucketResponse;
-import com.oracle.bmc.objectstorage.responses.GetNamespaceResponse;
-import com.oracle.bmc.objectstorage.responses.ListBucketsResponse;
-import com.oracle.bmc.workrequests.WorkRequestClient;
-import com.oracle.bmc.workrequests.requests.GetWorkRequestRequest;
-import com.oracle.bmc.workrequests.responses.GetWorkRequestResponse;
-import com.oracle.dragon.util.ADBRESTService;
-import com.oracle.dragon.util.Console;
 import com.oracle.dragon.util.DSSession;
-import com.oracle.dragon.util.ZipUtil;
 import com.oracle.dragon.util.exception.DSException;
-import com.oracle.dragon.util.exception.MissingDatabaseNameParameterException;
-import com.oracle.dragon.util.exception.UnsupportedPlatformException;
-
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.time.Duration;
-import java.util.Set;
-import java.util.TreeSet;
 
 import static com.oracle.dragon.util.Console.*;
 
@@ -78,17 +42,17 @@ public class DragonStack {
                 println("Timeout: " + e.isTimeout());
                 println("Client Side: " + e.isClientSide());
                 System.err.printf("ERROR: %s\n", e.getLocalizedMessage());
+                println(Style.ANSI_RED + "\n================================================================================");
+                println(Style.ANSI_RED + "Unhandled exception:");
+                e.printStackTrace(System.err);
             }
-        }
-        catch( DSException e ) {
-            e.displayMessageAndExit(Style.ANSI_BLUE + "duration: " + getDurationSince(totalDuration)+Style.ANSI_RESET);
-        }
-        catch (Exception e) {
-            println(Style.ANSI_RED+"================================================================================");
-            println(Style.ANSI_RED+"Unhandled exception:");
+        } catch (DSException e) {
+            e.displayMessageAndExit(Style.ANSI_BLUE + "duration: " + getDurationSince(totalDuration) + Style.ANSI_RESET);
+        } catch (Exception e) {
+            println(Style.ANSI_RED + "\n================================================================================");
+            println(Style.ANSI_RED + "Unhandled exception:");
             e.printStackTrace(System.err);
-        }
-        finally {
+        } finally {
             println(Style.ANSI_BLUE + "duration: " + getDurationSince(totalDuration));
         }
     }
