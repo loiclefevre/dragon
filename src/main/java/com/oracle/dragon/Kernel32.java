420170e9e82a88d5648d5e96e9d04ebb0cb08fda
Loïc Lefèvre <loic.lefevre@gmail.com>
2020-11-04 21:23:19 +0100

    version 2.0.2

--- src/main/java/com/oracle/dragon/Kernel32.java
+++ src/main/java/com/oracle/dragon/Kernel32.java
@@ -1,7 +1,9 @@
 package com.oracle.dragon;
 
 import com.oracle.dragon.util.Console;
+import org.graalvm.nativeimage.ImageInfo;
 import org.graalvm.nativeimage.Platform;
+import org.graalvm.nativeimage.Platforms;
 import org.graalvm.nativeimage.c.CContext;
 import org.graalvm.nativeimage.c.function.CFunction;
 import org.graalvm.word.Pointer;
@@ -12,37 +14,43 @@ import java.util.Collections;
 import java.util.List;
 
 @CContext(Kernel32.Directives.class)
-//@Platforms({Platform.WINDOWS.class, Platform.WINDOWS_AMD64.class, Platform.LINUX.class, Platform.LINUX_AMD64.class, Platform.DARWIN.class, Platform.DARWIN_AMD64.class})
 public class Kernel32 {
 
     public static void init() {
-        final String osName = System.getProperty("os.name").toLowerCase();
-        if (osName.startsWith("windows")) {
-            final BigDecimal OSVersion = new BigDecimal(System.getProperty("os.version"));
-            final String VMName = System.getProperty("java.vm.name");
-            if (OSVersion.doubleValue() >= 10.0d || (OSVersion.doubleValue() >= 6.2d && "Substrate VM".equalsIgnoreCase(VMName))) {
+        if (ImageInfo.inImageRuntimeCode()) {
+            final String osName = System.getProperty("os.name").toLowerCase();
+            if (osName.startsWith("windows")) {
+                final BigDecimal OSVersion = new BigDecimal(System.getProperty("os.version"));
+                final String VMName = System.getProperty("java.vm.name");
+                if (OSVersion.doubleValue() >= 10.0d || (OSVersion.doubleValue() >= 6.2d && "Substrate VM".equalsIgnoreCase(VMName))) {
 //                System.out.println("os.version: OK");
-                try {
-                    final Pointer handle = Kernel32.getStdHandle(-11);
+                    try {
+                        final Pointer handle = Kernel32.getStdHandle(-11);
 //                    System.out.println("Kernel32.getStdHandle: OK");
-                    Kernel32.setConsoleMode(handle, 7);
+                        Kernel32.setConsoleMode(handle, 7);
 //                    System.out.println("Kernel32.setConsoleMode: OK");
-                    Console.ENABLE_COLORS = true;
+                        Console.ENABLE_COLORS = true;
 //                    System.out.println("Console.ENABLE_COLORS: "+Console.ENABLE_COLORS);
 //                    System.out.println("Colors for Windows 10+ console enabled!");
-                } catch (Throwable t) {
-                    Console.ENABLE_COLORS = false;
+                    } catch (Throwable t) {
+                        Console.ENABLE_COLORS = false;
 //                    t.printStackTrace();
 //                    System.out.println("Console.ENABLE_COLORS: "+Console.ENABLE_COLORS);
+                    }
                 }
             }
         }
     }
 
+    @Platforms(Platform.WINDOWS.class)
     static final class Directives implements CContext.Directives {
         @Override
         public List<String> getHeaderFiles() {
-            return Collections.singletonList("<windows.h>");
+            if (Platform.includedIn(Platform.WINDOWS.class)) {
+                return Collections.singletonList("<windows.h>");
+            } else {
+                throw new IllegalStateException("Unsupported OS");
+            }
         }
 
         @Override
